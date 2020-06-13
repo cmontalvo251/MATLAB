81db63c24318ce499419663f97848ce3048b4591
Carlos Montalvo <cmontalvo@southalabama.edu>
2020-06-13 06:59:02 -0500

    Added an ECI frame to the texture mapp

--- Screen_Cast_Codes/Image_Example/texture_mapping.m
+++ Screen_Cast_Codes/Image_Example/texture_mapping.m
@@ -7,13 +7,25 @@ close all
 original = imread('world-map.jpg');
 imshow(original)
 
-%%%PLOT A SPHERE
-[X,Y,Z] = ellipsoid(0,0,0,10,10,10);
+%%%CREATE A SPHERE
+R = 10; %%%You can change this to the actual radius of the Earth
+N = 24; %%%Number of points to use if you set this to 24 then the longitudes will
+%%%be every 15 degrees
+[X,Y,Z] = ellipsoid(0,0,0,R,R,R,N);
 
 figure()
-surf(X,Y,-Z)
+surf(X,Y,-Z) %%FLIP THE Z-AXIS
 h = findobj('Type','surface'); %h is a handle - object oriented programming
-set(h,'CData',original,'FaceColor','texturemap','edgecolor','none')
+%set(h,'CData',original,'FaceColor','texturemap','edgecolor','none')
+set(h,'CData',original,'FaceColor','texturemap') %%%Use this if you want to plot the longitudes
+
+%%%%Let's make sure the texture is aligned with the ECI frame of the Earth
+hold on
+plot3([0 2*R],[0 0],[0 0],'b-','LineWidth',10) %%%X Axis
+plot3([0 0],[0 2*R],[0 0],'r-','LineWidth',10) %%%Y Axis
+plot3([0 0],[0 0],[0 2*R],'g-','LineWidth',10) %%%Z Axis
+
+return
 grid off
 axis off
 El = 24;
