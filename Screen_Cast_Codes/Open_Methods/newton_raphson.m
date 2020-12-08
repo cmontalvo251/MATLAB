c2ca9e884b9947f82d0f314dfdbbeb0406a9f434
Carlos Montalvo <cmontalvo@southalabama.edu>
2020-12-08 10:09:30 -0600

    Added new newton raphson with multiple guesses

--- Screen_Cast_Codes/Open_Methods/newton_raphson.m
+++ Screen_Cast_Codes/Open_Methods/newton_raphson.m
@@ -1,4 +1,4 @@
-function x0 = newton_raphson()
+function x0 = newton_raphson();
 clear
 clc
 
@@ -6,28 +6,40 @@ x0 = 10;
 
 iter = 0;
 
-alfa = 1.0;
+alfa = 0.1;
 
-while abs(f(x0)) > 1e-5
+err = abs(f(x0));
+
+while err > 1e-5
     
     iter = iter + 1;
 
     %%%Finds the zero
-    %x1 = x0 - alfa*f(x0)/fprime(x0)
+    x1 = x0 - alfa*f(x0)/fprime(x0);
     %%%Finds the minimum
-    x1 = x0 - alfa*fprime(x0)/fdblprime(x0)
+    %x1 = x0 - alfa*fprime(x0)/fdblprime(x0);
     
     x0 = x1;
 
+	err = abs(f(x0));
+
+	%disp([num2str(x0),' ',num2str(err)])
+
+	%pause
+
 end
+
+disp(['x0 = ',num2str(x0),' ','err = ',num2str(err),' iters = ',num2str(iter)])
 
 function out = f(in)
 
-out = in^3 - 1; %%%x = 1 leads to y = 0
+%out = in*(in-1)*(in+1); %%%x = 1 leads to y = 0
+%out = in*(in^2-1)
+out = in^3 - in;
 
 function out = fprime(in)
 
-out = 3*in^2;
+out = 3*in^2-1;
 
 function out = fdblprime(in)
 
