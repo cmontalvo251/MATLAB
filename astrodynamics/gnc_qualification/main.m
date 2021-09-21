7bf4c2650c63292e56e8905484d83e3c6de56327
Carlos Montalvo MSI Prestige <cmontalvo251@gmail.com>
2021-09-21 13:24:36 -0500

    orbit model added

--- astrodynamics/gnc_qualification/main.m
+++ astrodynamics/gnc_qualification/main.m
@@ -3,3 +3,12 @@ clc
 close all
 
 disp('GNC QUALIFICATION')
+
+%%USer defined parameters
+apogee = 1000; %%km above surface
+perigee = 1000; %km above surface
+N = 1000;
+
+%%%Run the orbit model
+[x,y,z,t,T_orbit] = orbit_model(apogee,perigee,N,1);
+disp(['Orbit Time = ',num2str(T_orbit)])
