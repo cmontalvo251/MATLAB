02d13a9e912ca8e88d66e9142a08dfb1d93fdad2
Carlos Montalvo T540p <cmontalvo251@gmail.com>
2021-09-29 10:08:41 -0500

    Added magnetic field and TIB to astrodynamics

---
+++ astrodynamics/gnc_qualification/magnetic_field.m
@@ -0,0 +1,28 @@
+function [BxI,ByI,BzI] = magnetic_field(x,y,z)
+%%%Make dummy variables
+BxI = 0*x;
+ByI = BxI;
+BzI = BxI;
+
+%%%Distance from the center of the Earth
+rho = sqrt(x.^2+y.^2+z.^2);
+
+%%%Call the magnetic field model
+%%%Convert Cartesian x,y,z into Lat,Lon, Alt
+phiE = 0;
+thetaE = acos(z./rho);
+psiE = atan2(y,x);
+latitude = 90-thetaE*180/pi;
+longitude = psiE*180/pi;
+rhokm = (rho)/1000;
+for idx = 1:length(x)
+	[BN,BE,BD] = igrf('28-Sep-2019',latitude(idx),longitude(idx),rhokm(idx),'geocentric'); 
+	%%%Convert NED (North East Down to X,Y,Z in ECI frame)
+	%%%First we need to create a rotation matrix from the NED frame to the 
+	%%%inertial frame
+	BNED = [BN;BE;BD]; 
+	BI = TIB(phiE,thetaE(idx)+pi,psiE(idx))*BNED;
+	BxI(idx) = BI(1);
+	ByI(idx) = BI(2);
+	BzI(idx) = BI(3);
+end
--- astrodynamics/gnc_qualification/main.m
+++ astrodynamics/gnc_qualification/main.m
@@ -7,15 +7,26 @@ disp('GNC QUALIFICATION')
 %%USer defined parameters
 apogee = 1000; %%km above surface
 perigee = 100000; %km above surface
-N = 100000;
+N = 10000;
 
 %%%Run the orbit model
 [x,y,z,t,T_orbit] = orbit_model(apogee,perigee,N,0);
 disp(['Orbit Time = ',num2str(T_orbit)])
 
+%%%Plot the orbit
 plot3(x,y,z)
-
+%%%Plot Altitude
 altitude = sqrt(x.^2 + y.^2 + z.^2);
-
 figure()
 plot(altitude)
+
+%%%Call the magnetic field model
+addpath('../igrf') %%%Hey you need to make sure you download igrf from mathworks
+[BxI,ByI,BzI] = magnetic_field(x,y,z);
+
+%%%Plot the magnetic field
+figure()
+plot(t,BxI,'b-')
+hold on
+plot(t,ByI,'r-')
+plot(t,BzI,'g-')
